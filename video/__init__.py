24874c4333377f622256f2e6ed72833397e996a0
caioviel <caioviel@gmail.com>
2015-10-09 14:49:14 -0300

    First Commit. Code from Multimedia Classes.

---
+++ video/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'caioviel'
+
+
+import videocut, videonorm, avconv
+
+normalize_video_lenght = videonorm.normalize_video_lenght
+
+cut_video_segment = videocut.cut_video_segment
+
+AVConverter = avconv.AVConverter
